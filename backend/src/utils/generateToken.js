695c2ec24a7239f638dbe76405eaeb155a1008f3
sharath <sharathshetty301@gmail.com>
2025-09-18 21:54:30 +0530

    jwt

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,8 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userid, name, email) => {
-  return jwt.sign({ userid, name, email }, process.env.ACCESS_TOKEN_SECRET, {
+  return jwt.sign({ userid, name, email }, process.env.JWT_SECRET, {
     expiresIn: "15m",
   });
 };
-
