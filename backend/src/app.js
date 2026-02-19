5f045e10f7364c90b423f4de9df22504279899e7
sharath <sharathshetty301@gmail.com>
2026-02-19 08:29:05 +0530

    updated files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import notesRoutes from "./routes/note.route.js";
 import authRoutes from "./routes/auth.route.js";
 import rateLimiter from "./middleware/rateLimiter.middleware.js";
 import path from "path";
+
+dotenv.config();
+
 import { startCleanupJob } from "./utils/cleanup.js";
 
 const app = express();
@@ -19,21 +23,21 @@ startCleanupJob();
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      withCredentials: true,
-    }),
-  );
-}
+process.env.NODE_ENV = "production";
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
+} else {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
 }
 
 app.get("/", (req, res) => {
